fdc996c4aa3ac32bf366ac960f1ebf3b7c8fe635
tej-thambi <tejthambi@gmail.com>
2023-05-25 20:37:58 -0700

    Initial Commit

---
+++ poppin-swift-assessment/poppin_swift_assessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  poppin_swift_assessmentApp.swift
+//  poppin-swift-assessment
+//
+//  Created by Tej Thambi on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct poppin_swift_assessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
